936e55f08e342ab9b877ed042614df20d4861e24
leanghok <leanghokoeng5@gmail.com>
2025-01-20 14:56:01 +0700

    add zoxide to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,20 @@
-if status is-interactive
-  set -Ux fish_greeting
+set -Ux fish_greeting
 
-  export GOROOT="/usr/local/go/"
-  export GOPATH="$HOME/go"
-  export PATH="$HOME/.tmuxifier/bin:$PATH"
-  export PATH="$HOME/.cargo/bin:$PATH"
-  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
-  export PATH="$HOME/.local/scripts:$PATH"
-  export EDITOR='nvim'
-  export MANPAGER="nvim +Man!"
-end
+export GOROOT="/usr/local/go/"
+export GOPATH="$HOME/go"
+export PATH="$HOME/.tmuxifier/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+export PATH="$HOME/.local/scripts:$PATH"
+export EDITOR='nvim'
+export MANPAGER="nvim +Man!"
 
 # pnpm
 set -gx PNPM_HOME "/home/leanghok/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
 
 fish_add_path /home/leanghok/.spicetify
+
+zoxide init fish | source
