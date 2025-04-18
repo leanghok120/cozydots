cd8cb95645450cf507d0e1d81f57f69b520f0b25
leanghok <leanghokoeng5@gmail.com>
2025-04-18 16:52:46 +0700

    add lsp and completions

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = { "lua_ls", "gopls", "ts_ls", "clangd", "pyright", "tailwindcss", "astro", "svelte" },
+      }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local lspconfig = require "lspconfig"
+      local mason_lspconfig = require "mason-lspconfig"
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      mason_lspconfig.setup_handlers {
+        function(server_name)
+          lspconfig[server_name].setup {
+            capabilities = capabilities,
+          }
+        end,
+      }
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end,
+  },
+}
