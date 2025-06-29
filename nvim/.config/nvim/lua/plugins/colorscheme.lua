48946328aa8654d485ace18d440020ada0cb040a
leanghok <leanghokoeng5@gmail.com>
2025-06-29 17:10:23 +0700

    change to wayland

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
-  config = function()
-    vim.g.gruvbox_material_foreground = "original"
-    vim.g.gruvbox_material_background = "hard"
-    vim.cmd.colorscheme "gruvbox-material"
-  end,
+  config = function ()
+    vim.cmd.colorscheme "catppuccin-mocha"
+  end
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -19,18 +19,6 @@ return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-      local lspconfig = require "lspconfig"
-      local mason_lspconfig = require "mason-lspconfig"
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          lspconfig[server_name].setup {
-            capabilities = capabilities,
-          }
-        end,
-      }
-
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
