1c4451e34308bde97f8baa5bfa92d16f009c53f1
leanghok <leanghokoeng5@gmail.com>
2025-09-16 08:35:51 +0700

    make dmenu case insensitive

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-i", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *flameshotcmd[] = { "flameshot", "gui", NULL };
 static const char *incvolcmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%", NULL };
